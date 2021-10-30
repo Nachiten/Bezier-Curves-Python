b88d56418c6a0ba936468839cb72fba0a6a246e7
Ignacio Baptista [Notebook] <ibaptista@frba.utn.edu.ar>
2021-10-30 12:42:38 -0300

    Commit inicial. Creado repo y adaptadas resoluciones

---
+++ main.py
@@ -0,0 +1,124 @@
+from curves import *
+
+width, height = 1920, 1080
+size = (1366, 768)
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+# colors
+white = (235, 235, 235)
+black = (20, 20, 20)
+red = (242, 2, 2)
+green = (2, 242, 102)
+blue = (2, 146, 242)
+purple = (205, 163, 255)
+
+showLinear = True
+showQuadratic = True
+showCubic = True
+
+# parameters
+t = 0
+speed = 0.002
+
+linear_positions = []
+Quadratic_positions = []
+cubic_positions = []
+
+if showLinear:
+    linear_positions = [Position(50, 600, "P0"),
+                        Position(250, 200, "P1")]
+if showQuadratic:
+    Quadratic_positions = [Position(460, 600, "P0"),
+                           Position(580, 450, "P1"),
+                           Position(520, 200, "P2")]
+if showCubic:
+    cubic_positions = [Position(1050 - 300, 600, "P0"),
+                       Position(1280 - 300, 200, "P1"),
+                       Position(1420 - 300, 600, "P2"),
+                       Position(1600 - 300, 200, "P3")]
+
+quadratic_curve = []
+cubic_curve = []
+curve1 = []
+curve2 = []
+curve3 = []
+
+run = True
+while run:
+    screen.fill(white)
+    clock.tick(fps)
+    frameRate = int(clock.get_fps())
+    pygame.display.set_caption("Bezier Curve - FPS : {}".format(frameRate))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    # Gui Text
+    text = font.render(" T = " + str(t)[:5], True, black)
+    textRect = text.get_rect()
+    textRect.center = (960, 100)
+    screen.blit(text, textRect)
+    linear = font.render("Linear ", True, black)
+
+    textRect = linear.get_rect()
+    textRect.center = (240, 120)
+    screen.blit(linear, textRect)
+    Quadratic = font.render("Quadratic", True, black)
+    textRect = Quadratic.get_rect()
+    textRect.center = (620, 120)
+    screen.blit(Quadratic, textRect)
+
+    cubic = font.render("Cubic", True, black)
+    textRect = cubic.get_rect()
+    textRect.center = (1400, 120)
+    screen.blit(cubic, textRect)
+
+    # Line Separators ---- | ----- | ------
+    posXLine1 = 380
+    posXLine2 = 700
+    pygame.draw.line(screen, purple, (posXLine1, 700), (posXLine1, 150), 1)
+    pygame.draw.line(screen, purple, (posXLine2, 700), (posXLine2, 150), 1)
+
+    LinearCurve(linear_positions, t, screen, red)
+    QuadraticCurve(Quadratic_positions, t, screen, red, quadratic_curve, green)
+    CubicCurve(cubic_positions, t, screen, red, cubic_curve, green, blue, curve1, curve2, curve3)
+
+    if len(cubic_curve) > 2:
+        pygame.draw.lines(screen, (179, 179, 179), False, curve1, 3)
+        pygame.draw.lines(screen, (179, 179, 179), False, curve3, 3)
+        pygame.draw.lines(screen, (179, 179, 179), False, curve2, 3)
+        pygame.draw.lines(screen, red, False, cubic_curve, 5)
+
+    if len(quadratic_curve) > 2:
+        pygame.draw.lines(screen, red, False, quadratic_curve, 5)
+
+    if t >= 1:
+        t = 0
+        quadratic_curve.clear()
+        cubic_curve.clear()
+        curve1.clear()
+        curve2.clear()
+        curve3.clear()
+
+    # draw points
+    for point in linear_positions:
+        point.display(screen, black)
+    for point in Quadratic_positions:
+        point.display(screen, black)
+    for point in cubic_positions:
+        point.display(screen, black)
+
+    t += speed
+    pygame.display.update()
+
+pygame.quit()
